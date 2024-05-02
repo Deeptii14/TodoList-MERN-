825bcfd38fb8307f389481321ceaa01ab59f33e5
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-05-02 00:16:21 -0500

    Refactor functions to use ES6 module syntax
    Updated import statements to use ES6 syntax
    
    Exported functions  as named exports for better code organization
    
    Improved code readability and maintainability

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -1,52 +1,48 @@
-const User= require("../../models/user");
-const bcrypt = require("bcrypt");
-exports.signup = async (req, res) => {
-    try {
-      //data fetch
-      const {
-        email,
-        password,
-        userName
-      } = req.body;
-  
-      //validate
-      if (
-        !password ||
-        !email ||
-        !userName
-      ) {
-        return res.status(403).json({
-          success: false,
-          message: "All fields are required",
-        });
-      }
-      //existing user
-      const existinguser = await User.findOne({ email });
-      if (existinguser) {
-        return res.status(400).json({
-          success: false,
-          message: "User is already registered!",
-        });
-      }
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      const user = await User.create({
-        email,
-        password: hashedPassword,
-        userName:userName
-      });
-      return res.status(200).json({
-        success: true,
-        message: "User Created Successfully!",
-        user,
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
+import User from "../../models/user.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  try {
+    // Data fetch
+    const { email, password, userName } = req.body;
+
+    // Validate
+    if (!password || !email || !userName) {
+      return res.status(403).json({
         success: false,
-        message: error.message,
+        message: "All fields are required",
       });
     }
-  };
-  
- 
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User is already registered!",
+      });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      userName,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User Created Successfully!",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
